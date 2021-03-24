02d1a39fa07b3f81fc6811feaf69a20604002d0f
felipez <tifonzafel@gmail.com>
2021-03-24 15:42:47 +0100

    fix typo in install requires

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name='equivariant-attention',
       version='1.1',
       packages=find_packages(),
-      install_require=[
-          'lie_learn@git+https://github.com/AMLab-Amsterdam/lie_learn@master',
+      install_requires=[
+          'lie_learn@git+https://github.com/AMLab-Amsterdam',
           'numpy'
       ]
       )
