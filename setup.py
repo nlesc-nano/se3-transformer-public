4bab0761f491bab1d1a5a3ad3099e8702c9ba556
felipez <tifonzafel@gmail.com>
2021-03-24 12:57:23 +0100

    improve setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='equivariant-attention', version='1.0', packages=find_packages())
+setup(name='equivariant-attention',
+      version='1.1',
+      packages={'equivariant_attention': 'equivariant_attention'},
+      install_require=[
+          'lie_learn@git+https://github.com/AMLab-Amsterdam/lie_learn',
+          'numpy'
+      ]
+      )
