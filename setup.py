ac5d48227f847ec3254be55e26df6ad88d3cc1c1
felipez <tifonzafel@gmail.com>
2021-03-24 15:49:52 +0100

    fix lie_learn installation

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+try:
+    import dgl
+except ImportError:
+    raise ImportError("The DGL module is required: https://www.dgl.ai/")
+
+
 setup(name='equivariant-attention',
       version='1.1',
       packages=find_packages(),
       install_requires=[
-          'lie_learn@git+https://github.com/AMLab-Amsterdam',
-          'numpy'
+          'lie_learn@git+https://github.com/AMLab-Amsterdam/lie_learn',
+          'numpy', 'scipy'
       ]
       )
